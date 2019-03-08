d10ce29c2fddbdc09a1ba3c47bb4e2dcc7f33b04
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-03-08 20:30:31 +0530

    corrected the permissions

--- data.tf
+++ data.tf
@@ -5,7 +5,7 @@ data "aws_iam_policy_document" "route53_query_logs" {
       "logs:PutLogEvents",
     ]
 
-    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/${var.domain_name}"]
+    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/${var.domain_name}/*"]
 
     principals {
       identifiers = ["route53.amazonaws.com"]
