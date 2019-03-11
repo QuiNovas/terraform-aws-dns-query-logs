664f685ffb16a6da745876cee2e2bf230f0518be
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2019-03-11 15:24:55 +0530

    correction to iam, to narrow it

--- data.tf
+++ data.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "route53_query_logs" {
   statement {
     actions = [
@@ -5,7 +7,7 @@ data "aws_iam_policy_document" "route53_query_logs" {
       "logs:PutLogEvents",
     ]
 
-    resources = ["arn:aws:logs:*:*:log-group:/aws/route53/${var.domain_name}/*"]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/route53/${var.domain_name}:*"]
 
     principals {
       identifiers = ["route53.amazonaws.com"]
